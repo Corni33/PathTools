932305c16d786ed15d0d35a94a981dba5b88a48d
Tobias Brunner <tobiausfeu@googlemail.com>
2018-03-27 22:00:46 +0200

    using Eigen for vectorized processing

--- PathTools.h
+++ PathTools.h
@@ -1,16 +1,17 @@
 /**
   PathTools.h
-  Collection of datatypes and template functions for processing 2D points and paths 
+  Collection of datatypes and templated functions for processing 2D points and paths 
 
-  @version 0.1 2018/02/17
+  @version 0.2 2018/03/27
 */
 
 #pragma once
 
 #include <vector>
 #include <cmath>
-#include <limits>
 #include <iostream>
+
+#include <Eigen/Core>
 
 
 namespace PathTools
@@ -24,9 +25,10 @@ struct Point
 {
   T x, y;
 
-  Point() : x(0), y(0) {} 
+  Point() : x(static_cast<T>(0)), y(static_cast<T>(0)) {}
   Point(T x_, T y_) : x(x_), y(y_) {}  
 };
+
 
 /**
   Point on a Path
@@ -37,7 +39,7 @@ struct PathPoint
   size_t ind;
   T u;
 
-  PathPoint() : ind(0), u(0) {}
+  PathPoint() : ind(0), u(static_cast<T>(0)) {}
   PathPoint(size_t ind_, T u_) : ind(ind_), u(u_) {}
 };
 
@@ -48,35 +50,37 @@ template <typename T>
 class Path
 {
 public:
-  std::vector<Point<T>> points;
-  std::vector<T> length;
+  void add_point(const T x, const T y);
+  void add_point(const Point<T> p); 
+
+  const std::vector<T>& x_points() { return m_x; }
+  const std::vector<T>& y_points() { return m_y; }
 
   /**
-  Pre-calculates the path length to each point of the path
-  */
-  void          prepare(void);
-
-  /**
-  Caclulates the coordinates of a PathPoint on this path
-  @param PathPoint  
-  @return Point containing the coordinates of the PathPoint
+  Caclulates the x/y-coordinates of a "PathPoint" on this path
+    @param PathPoint  
+    @return Point containing the coordinates of the PathPoint
   */
   Point<T>      coordinates(PathPoint<T> const& pp);
 
   /**
-  Caclulates the closest PathPoint on the path to Point p
-  @param p: Point to which the closest PathPoint will be calculated
-  @return closest PathPoint
+  Caclulates the closest "PathPoint" to Point p
+    @param p: Point to which the closest "PathPoint" will be calculated
+    @return closest "PathPoint"
   */
   PathPoint<T>  closest_point(Point<T> const& p);
 
   /**
   Moves a desired distance along the path and returns the resulting PathPoint
-  @param start_point: PathPoint from which the movement will start
-  @param dist_desired: Desired moving distance along the path
-  @return PathPoint that is reached after the movement
+    @param start_point: PathPoint from which the movement will start
+    @param dist_desired: Desired moving distance along the path
+    @return PathPoint that is reached after the movement
   */
   PathPoint<T>  move_along(PathPoint<T> const& pp_start, T const dist_desired);
+
+private:
+  std::vector<T> m_x, m_y, m_vx, m_vy, m_dst_sq, m_p_length, m_u;
+
 };
 
 /**
@@ -88,6 +92,9 @@ public:
 template <typename T>
 T dist_2d_squared(Point<T> const& p, Point<T> const& q);
 
+template <typename T>
+T dist_2d_squared(const T x0, const T y0, const T x1, const T y1);
+
 /**
 Calculates the distance between two 2D points
 @param p First point
@@ -97,18 +104,12 @@ Calculates the distance between two 2D points
 template <typename T>
 T dist_2d(Point<T> const& p, Point<T> const& q);
 
-/**
-Calculates the index of the path point which is closest to a given point
-@param path  
-@param p Point, to which the closest path point should be calculated
-@return Index of the path point which is closest to p
-*/
 template <typename T>
-size_t closest_point_index(Path<T> const& path, Point<T> const& p);
+T dist_2d(const T x0, const T y0, const T x1, const T y1);
 
 
 /*****************************************************************************************************
-Overloading of basic operators for data type "Point"
+Overloading of some basic operators 
 ******************************************************************************************************/
 
 template <typename T>
@@ -152,10 +153,61 @@ Function implementations
 ******************************************************************************************************/
 
 template <typename T>
+void Path<T>::add_point(const T x, const T y)
+{
+  if (m_x.size() > 0)
+  {
+    T last_x = m_x.back();
+    T last_y = m_y.back();
+
+    auto ind_last = m_x.size() - 1;
+
+    m_x.push_back(x);
+    m_y.push_back(y);
+
+    // vector pointing from last point to current point
+    m_vx[ind_last] = x - last_x;
+    m_vx.push_back(0);
+    m_vy[ind_last] = y - last_y;
+    m_vy.push_back(0);
+
+    // squared distance between last point and current point
+    m_dst_sq[ind_last] = dist_2d_squared(x, y, last_x, last_y);
+    m_dst_sq.push_back(0);
+
+    // distance from beginning of path until current point
+    m_p_length[ind_last] += dist_2d(x, y, last_x, last_y);
+    m_p_length.push_back(m_p_length.back());
+  }
+  else
+  {
+    m_x.push_back(x);
+    m_y.push_back(y);
+    m_vx.push_back(0);
+    m_vy.push_back(0);
+    m_dst_sq.push_back(0);
+    m_p_length.push_back(0);
+  }
+
+  m_u.push_back(0);
+}
+
+template <typename T>
+void Path<T>::add_point(const Point<T> p)
+{ 
+  add_point(p.x, p.y);
+}
+
+template <typename T>
 T dist_2d_squared(Point<T> const& p, Point<T> const& q)
 {
-  Point<T> tmp = p - q;
-  return tmp.x*tmp.x + tmp.y*tmp.y;
+  return dist_2d_squared(p.x, p.y, q.x, q.y);
+}
+
+template <typename T>
+T dist_2d_squared(const T x0, const T y0, const T x1, const T y1)
+{
+  return (x1 - x0)*(x1 - x0) + (y1 - y0)*(y1 - y0);
 }
 
 template <typename T>
@@ -165,27 +217,12 @@ T dist_2d(Point<T> const& p, Point<T> const& q)
 }
 
 template <typename T>
-size_t closest_point_index(Path<T> const& path, Point<T> const& p)
+T dist_2d(const T x0, const T y0, const T x1, const T y1)
 {
-  size_t closest_point_index = 0, ind = 0;
-  T shortest_dist = std::numeric_limits<T>::max();
-  T current_dist = T();
-
-  for (Point<T> const& q : path.points)
-  {
-    current_dist = dist_2d_squared(p, q);
-    if (dist_2d_squared(p, q) < shortest_dist)
-    {
-      shortest_dist = current_dist;
-      closest_point_index = ind;
-    }
-    ++ind;
-  }
-
-  return closest_point_index;
+  return std::sqrt(dist_2d_squared(x0, y0, x1, y1));
 }
 
-template <typename T>
+/*template <typename T>
 void closest_point_on_line_segment(Point<T> const& a, Point<T> const& b, Point<T> const& p, Point<T>& closest_point, T& interp_factor)
 {
   Point<T> v = b - a; 
@@ -201,118 +238,57 @@ void closest_point_on_line_segment(Point<T> const& a, Point<T> const& b, Point<T
  
   T u = ((p.x - a.x) * v.x + (p.y - a.y) * v.y) / dist_squared;
   
-  if (u > 1)
-  {
-    u = static_cast<T>(1);
-  }
-  else if (u < 0)
-  {
-    u = static_cast<T>(0);
-  }
+  u = std::min<T>(1, std::max<T>(u, 0)); 
 
   interp_factor = u;
   closest_point = a + u * v;  
   return;
-}
-
-template <typename T>
-void Path<T>::prepare()
-{
-  this->length.resize(this->points.size());
-  T dist = 0;
-
-  this->length[0] = 0;
-
-  for (size_t i = 0; i != this->points.size() - 1; ++i)
-  {
-    dist += dist_2d(this->points[i], this->points[i + 1]);
-    this->length[i + 1] = dist;
-  }
-
-  for (auto const& l : this->length)
-  {
-    std::cout << "prepare: dist = " << l << std::endl;
-  }
-  return;
-}
+}*/
 
 template <typename T>
 Point<T> Path<T>::coordinates(PathPoint<T> const& pp)
 {
-  if (this->points.size() == 0 || pp.ind >= this->points.size())
+  size_t n_points = m_x.size();
+
+  if (n_points == 0 || pp.ind >= n_points)
   {
-    return Point<T>();
+    return Point<T>(0, 0);
   }
 
-  if (this->points.size() == 1)
+  if (n_points == 1)
   {
-    return this->points[0];
+    return Point<T>(m_x[0], m_y[0]);
   }
 
-  if (this->points.size() - 1 == pp.ind)
+  if (n_points - 1 == pp.ind)
   {
-    return this->points[pp.ind];
+    return Point<T>(m_x[pp.ind], m_y[pp.ind]);
   }
 
-  return this->points[pp.ind] + pp.u * (this->points[pp.ind + 1] - this->points[pp.ind]);
+  return Point<T>(m_x[pp.ind] + pp.u * m_vx[pp.ind], m_y[pp.ind] + pp.u * m_vy[pp.ind]);
 }
 
 template <typename T>
 PathPoint<T> Path<T>::closest_point(Point<T> const& p)
-{
-  PathPoint<T> pp;
+{ 
+  using array_map = Eigen::Map<Eigen::Array<T, -1, 1>>;
 
-  size_t const path_size = this->points.size();
-
-  if (path_size <= 1)
-  {    
-    return pp; // ind=0, u=0
-  }
-
-  size_t ind_closest = closest_point_index(*this, p);
-  std::cout << "ind closest: " << ind_closest << std::endl;
-  Point<T> p_temp;
-
-  if (ind_closest == 0)
-  {
-    pp.ind = 0;
-    closest_point_on_line_segment(this->points[0], this->points[1], p, p_temp, pp.u);
-    return pp;
-  }
-
-  if (ind_closest == path_size - 1)
-  {    
-    closest_point_on_line_segment(this->points[path_size - 2], this->points[path_size - 1], p, p_temp, pp.u);
-    if (pp.u >= static_cast<T>(1.0))
-    {
-      pp.ind = path_size - 1;
-    }
-    else
-    {
-      pp.ind = path_size - 2;
-    }
-    return pp;
-  }
+  // wrap std::vector data into Eigen array maps
+  auto sz = m_x.size();
+  array_map x       (m_x.data(),      sz, 1);
+  array_map y       (m_y.data(),      sz, 1);
+  array_map vx      (m_vx.data(),     sz, 1);
+  array_map vy      (m_vy.data(),     sz, 1);
+  array_map dst_sq  (m_dst_sq.data(), sz, 1);
+  array_map u       (m_u.data(),      sz, 1);
   
-  T u_next;
+  // find minimum distance to every line segment of the path
+  u = (((p.x - x) * vx + (p.y - y) * vy)).min(dst_sq).max(0);
 
-  closest_point_on_line_segment(this->points[ind_closest], this->points[ind_closest + 1], p, p_temp, u_next); // next segment
-  T dist_next = dist_2d_squared(p, p_temp);
+  int ind;
+  (Eigen::square(x + vx * u - p.x) + Eigen::square(y + vy * u - p.y)).minCoeff(&ind);
 
-  closest_point_on_line_segment(this->points[ind_closest - 1], this->points[ind_closest], p, p_temp, pp.u); // previous segment
-  T dist_prev = dist_2d_squared(p, p_temp);  
-  
-  if (dist_next < dist_prev)
-  {
-    pp.u = u_next;
-    pp.ind = ind_closest;
-  }
-  else
-  {
-    pp.ind = ind_closest - 1;
-  }
-
-  return pp;
+  return PathPoint<T>(ind, dst_sq[ind] > 0.00001 ? u[ind] / dst_sq[ind] : 0);
 }
 
 template <typename T>
@@ -320,8 +296,8 @@ PathPoint<T> Path<T>::move_along(PathPoint<T> const& pp_start, T const dist_desi
 {
   PathPoint<T> pp_target = pp_start;
 
-  T length_seg = this->length[pp_start.ind + 1] - this->length[pp_start.ind];
-  T dist_moved = (static_cast<T>(1.0) - pp_start.u) * length_seg;
+  T length_seg = m_p_length[pp_start.ind + 1] - m_p_length[pp_start.ind];
+  T dist_moved = (1.0 - pp_start.u) * length_seg;
 
   if (dist_moved > dist_desired) 
   {
@@ -329,22 +305,23 @@ PathPoint<T> Path<T>::move_along(PathPoint<T> const& pp_start, T const dist_desi
     return pp_target;
   }
 
-  T offset = this->length[pp_start.ind + 1] - dist_moved;
+  T offset = m_p_length[pp_start.ind + 1] - dist_moved;
   T dist_desired_adapted = dist_desired + offset;
 
-  for (size_t i = pp_start.ind; i != this->length.size(); ++i)
+  size_t n_points = m_x.size();
+  for (size_t i = pp_start.ind; i != n_points; ++i) 
   {   
-    if (this->length[i] > dist_desired_adapted)
+    if (m_p_length[i] > dist_desired_adapted)
     {
-      T dist_seg_begin = this->length[i - 1];
-      pp_target.u = (dist_desired_adapted - dist_seg_begin) / (this->length[i] - this->length[i - 1]);
+      T dist_seg_begin = m_p_length[i - 1];
+      pp_target.u = (dist_desired_adapted - dist_seg_begin) / (m_p_length[i] - m_p_length[i - 1]);
       pp_target.ind = i - 1;
       return pp_target;
     }    
   }
 
   pp_target.u = 0;
-  pp_target.ind = this->length.size() - 1;
+  pp_target.ind = n_points - 1;
   return pp_target;
 }
 
